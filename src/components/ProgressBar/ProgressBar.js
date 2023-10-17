3e7a51afd054b69fda7b5bdbd1846ef37abcbd23
Michael Hewitt <hewitt@coda.io>
2023-10-17 13:41:08 -0700

    My progress bar with <div>

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,44 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const Component = Sizes[size];
+  return <Component role='progressbar' aria-valuenow={value.toString()}><Mask><Indicator width={value}/></Mask></Component>;
 };
+
+const Base = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  overflow: hidden;
+`
+
+const Large = styled(Base)`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  height: 24px;
+  border-radius: 8px;
+  padding: 4px;
+`
+
+const Medium = styled(Base)`
+  height: 12px;
+  border-radius: 4px;
+`
+
+const Small = styled(Base)`
+  height: 8px;
+  border-radius: 4px;
+`
+
+const Sizes = {large: Large, medium: Medium, small: Small};
+
+const Mask = styled.div`
+  height: 16px;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const Indicator = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${props => props.width}%;
+`
 
 export default ProgressBar;
