9aed29e4f24ab47f43430e94a247681b6eed9f0d
Michael Hewitt <hewitt@coda.io>
2023-10-18 14:43:27 -0700

    Eliminate progress bar styled subclasses in favor of a map to avoid double-setting the height on parent & child - child determines the height, parent does the padding - thus, the map connects styles across multiple styled elements

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,52 +5,55 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Sizes = {
+    large:
+        {
+            padding: 4,
+            borderRadius: 8,
+            barHeight: 16,
+        },
+    medium:
+        {
+            padding: 0,
+            borderRadius: 4,
+            barHeight: 12,
+        },
+    small:
+        {
+            padding: 0,
+            borderRadius: 4,
+            barHeight: 8,
+        }
+};
+
 const ProgressBar = ({ value, size }) => {
-  const Component = Sizes[size];
+  const {padding, borderRadius, barHeight} = Sizes[size];
   return (
-    <Component role='progressbar' aria-valuenow={value.toString()}>
+    <Wrapper padding={padding} borderRadius={borderRadius} role='progressbar' aria-valuenow={value.toString()}>
       <Mask>
         <VisuallyHidden>{value}%</VisuallyHidden>
-        <Bar width={value} />
+        <Bar width={value} height={barHeight} />
       </Mask>
-    </Component>
+    </Wrapper>
   );
 };
 
-const Base = styled.div`
+const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  overflow: hidden;
+  padding: ${props => props.padding}px;
+  border-radius: ${props => props.borderRadius}px;
 `
-
-const Large = styled(Base)`
-  height: 24px;
-  border-radius: 8px;
-  padding: 4px;
-`
-
-const Medium = styled(Base)`
-  height: 12px;
-  border-radius: 4px;
-`
-
-const Small = styled(Base)`
-  height: 8px;
-  border-radius: 4px;
-`
-
-const Sizes = {large: Large, medium: Medium, small: Small};
 
 const Mask = styled.div`
-  height: 16px;
   border-radius: 4px;
   overflow: hidden;
 `
 
 const Bar = styled.div`
   background-color: ${COLORS.primary};
-  height: 100%;
   width: ${props => props.width}%;
+  height: ${props => props.height}px;
 `
 
 export default ProgressBar;
